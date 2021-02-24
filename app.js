4f9b914e800ffe22de698e3b2e74cb30f8218947
David <juanlc1526@gmail.com>
2021-02-24 00:50:33 -0500

    first version of my portfolio

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-document.addEventListener('DOMContentLoaded', function() {
+(function($){
+  'use strict';
+    $(window).on('load', function () {
+        if ($("#preloader").length > 0)
+        {
+            $("#preloader").fadeOut("slow");
+        }
+    });
+})(jQuery)
 
-    window.addEventListener('scroll', myFunctionForSticky);
-    var navbar = document.getElementById("myHeader");
-    var sticky = navbar.offsetTop;
-  
-    function myFunctionForSticky() {
-      if (window.pageYOffset >= sticky) {
-        console.log("window.pageYOffset >= sticky");
-      } else {
-        console.log("Not window.pageYOffset >= sticky");
-      }
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
-})
+
 
 window.addEventListener('scroll', function(){
   let animation = document.getElementById('animated');
   let positionObj1 = animation.getBoundingClientRect().top;
-  console.log(positionObj1);
   let sizeOfScreen = window.innerHeight/1.1;
 
   if(positionObj1 < sizeOfScreen){
     animation.style.animation = "text 15s ease 0s"
   }
 })
+
+window.addEventListener('scroll', function(){
+  let myLogo = document.getElementById('logo');
+  let hideLogo = document.getElementById('welcome-title');
+  let positionLogo = hideLogo.getBoundingClientRect().top;
+  let screenSize = window.innerHeight/7;
+
+  if(positionLogo < screenSize){
+    myLogo.style.visibility = "hidden";
+  } else{
+    myLogo.style.visibility = "visible";
+  }
+})
+
+
+
+
+
+
+
+
+
+
